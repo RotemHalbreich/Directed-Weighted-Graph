ef7c5de9aab0dfad72f074c451dcc9fd0871e09b
Rotem Halbreich <rotem.halbreich@gmail.com>
2021-01-14 12:05:48 +0200

    Node test progress

--- Ex3/tests/NodeTest.py
+++ Ex3/tests/NodeTest.py
@@ -6,10 +6,28 @@ from Ex3.src.DiGraph import DiGraph
 class MyTestCase(unittest.TestCase):
 
     def test_add_inside(self):
-        pass
+        node1 = Node(1)
+        self.assertIsNotNone(node1)
+        node1.add_inside(2, 13)
+        node1.add_inside(3, 12)
+        self.assertEqual(2, len(node1.get_inside()))
+        node1.add_inside(124, 45687)
+        node1.add_inside(124, 45687)
+        node1.add_inside(124, 3)
+        self.assertEqual(3, len(node1.get_inside()))
 
-    def add_outside(self):
-        pass
+
+    def test_add_outside(self):
+        node1 = Node(1)
+        self.assertIsNotNone(node1)
+        node1.add_outside(5, 124)
+        node1.add_outside(7, 12)
+        self.assertEqual(2, len(node1.get_outside()))
+        node1.add_outside(35, 6)
+        node1.add_outside(35, 235)
+        self.assertEqual(3, len(node1.get_outside()))
+
+
 
     def test_remove_inside(self):
         pass
